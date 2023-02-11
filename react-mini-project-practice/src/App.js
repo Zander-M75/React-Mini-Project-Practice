6d44415c93a1e595e05cb7cdd921afe7dc3b253a
Zander <94405288+Zmarenberg@users.noreply.github.com>
2023-02-11 18:46:43 -0500

    added TODO modal and backdrop components

--- react-mini-project-practice/src/App.js
+++ react-mini-project-practice/src/App.js
@@ -1,9 +1,21 @@
 
 
+import Todo from "./components/Todo";
+
+
+
 function App() {
   return (
-    <div>Hello!</div>
+    <div>
+      <h1>My Todos</h1>
+      <Todo text='Learn React' />
+      <Todo text='Master React' />
+      <Todo text='Eat a sandwich' />
+
+    </div>
   );
 }
 
 export default App;
+
+
