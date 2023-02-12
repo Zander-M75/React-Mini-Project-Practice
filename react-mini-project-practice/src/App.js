88addf2bdb1471b3b4c5862c29ba4f6b76afdb77
Zander <94405288+Zmarenberg@users.noreply.github.com>
2023-02-11 19:44:47 -0500

    wiped out app.js and all components to start over :)

--- react-mini-project-practice/src/App.js
+++ react-mini-project-practice/src/App.js
@@ -1,18 +1,6 @@
-
-
-import Todo from "./components/Todo";
-
-
-
 function App() {
   return (
-    <div>
-      <h1>My Todos</h1>
-      <Todo text='Learn React' />
-      <Todo text='Master React' />
-      <Todo text='Eat a sandwich' />
-
-    </div>
+    <div></div>
   );
 }
 
