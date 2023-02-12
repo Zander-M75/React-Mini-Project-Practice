c588b0fd215848db62fdf33f26b933de002ae703
Zander <94405288+Zmarenberg@users.noreply.github.com>
2023-02-11 20:01:44 -0500

    included pages folder, created js filed for AllMeetups, NewMeetups, and Favorites

--- react-mini-project-practice/src/App.js
+++ react-mini-project-practice/src/App.js
@@ -1,6 +1,32 @@
+
+import { Route } from 'react-router-dom';
+
+import AllMeetupsPage from './pages/AllMeetups';
+import NewMeetupPage from './pages/NewMeetup';
+import FavoritesPage from './pages/Favorites';
+
+
 function App() {
+  // localhost:3000/ => AllMeetupsPage
+  // localhost:3000/new-meetup => NewMeetupPage
+  // localhost:3000/favorites => FavoritesPage
+
+
   return (
-    <div></div>
+    <div>
+      <Route path='/'>
+        <AllMeetupsPage />
+      </Route>
+
+      <Route path='/new-meetup'>
+        <NewMeetupPage />
+      </Route>
+
+      <Route path='/favorites'>
+        <FavoritesPage />
+      </Route>
+
+    </div>
   );
 }
 
